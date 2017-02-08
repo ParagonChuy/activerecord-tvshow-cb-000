4a8a78fc2b6d32e7f27e5e78882f01a84a7e8a8b
Jesus Vega <chuyspc@gmail.com>
2017-02-08 09:28:06 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,14 +1,18 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
+    Show.order('rating desc').limit(1)[0].rating
   end
 
   def self.most_popular_show
+    Show.order('rating desc').limit(1)[0]
   end
 
   def self.lowest_rating
+    Show.order('rating asc').limit(1)[0].rating
   end
 
   def self.least_popular_show
+    Show.order('rating asc').limit(1)[0]
   end
 
   def self.ratings_sum
